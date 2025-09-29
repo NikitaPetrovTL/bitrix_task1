9579768ac6ad626c8f34658a5ae55926ded0555c
NikitaPetrovTL <commonsensical.mind@gmail.com>
2025-09-29 09:24:21 +0700

    Delete result_modifier.php

--- result_modifier.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-foreach ($arResult["ITEMS"] as &$arItem) {
-    $arItem["DISPLAY_ACTIVE_FROM"] = $arItem["ACTIVE_FROM"] ? FormatDate("d.m.Y", MakeTimeStamp($arItem["ACTIVE_FROM"])) : "";
-}
-unset($arItem);
